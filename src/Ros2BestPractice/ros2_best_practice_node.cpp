0c45a000896246577b859455171c7120d709e93b
Thefaraday <cahmet644@gmail.com>
2022-08-23 00:28:53 +0300

    Auto variable is removed.

--- src/Ros2BestPractice/ros2_best_practice_node.cpp
+++ src/Ros2BestPractice/ros2_best_practice_node.cpp
@@ -3,9 +3,10 @@ int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     std::string t_node_name{"ros2_best_practice"};
-    auto first_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
 
-    // auto second_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
+    std::shared_ptr<ros2_cpp_pkg::Ros2BestPractice> first_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
+
+    // std::shared_ptr<ros2_cpp_pkg::Ros2BestPractice> second_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
 
     // auto third_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
 
