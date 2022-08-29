fbb71f245518303014da84171bfa1d5066dbf9d1
Thefaraday <cahmet644@gmail.com>
2022-08-29 16:20:47 +0300

    Optimization is updated.

--- src/Ros2BestPractice/ros2_best_practice_node.cpp
+++ src/Ros2BestPractice/ros2_best_practice_node.cpp
@@ -4,7 +4,9 @@ int main(int argc, char **argv)
     rclcpp::init(argc, argv);
     std::string t_node_name{"ros2_best_practice"};
 
-    std::shared_ptr<ros2_cpp_pkg::Ros2BestPractice> first_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
+    std::shared_ptr<ros2_cpp_pkg::Ros2BestPractice> first_node{nullptr};
+
+    first_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
 
     // std::shared_ptr<ros2_cpp_pkg::Ros2BestPractice> second_node = std::make_shared<ros2_cpp_pkg::Ros2BestPractice>(t_node_name);
 
