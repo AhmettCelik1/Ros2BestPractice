1d1a00d91a126aeb873dac1167eba0908f215430
Ahmet Çelik <96351757+AhmettCelik1@users.noreply.github.com>
2023-06-21 20:58:04 +0300

    Removed build files.

--- src/object_avoidance/build/object_avoidance/CMakeFiles/object_avoidance_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/object_avoidance_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/object_avoidance_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/object_avoidance/build/object_avoidance/CMakeFiles/object_avoidance_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for object_avoidance_uninstall.
-# This may be replaced when dependencies are built.
